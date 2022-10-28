d4a372b1101df7d808d49e65f853a36c53e90ddd
jalal-lagdimi <jalagdimi@gmail.com>
2022-10-28 11:48:21 +0100

    commit select dishes

--- logic.js
+++ logic.js
@@ -13,30 +13,66 @@ const allmenu =document.getElementById("categorie-all");
 const dishes =document.getElementById("catgorie-dishes");
 const drinks =document.getElementById("catgorie-drinks");
 const salad =document.getElementById("catgorie-salad");
+const alldishes =document.querySelectorAll(".men-content");
+
 
 dishes.addEventListener("click",()=> {
     dishes.classList.add("active");
     allmenu.classList.remove("active");
     drinks.classList.remove("active");
     salad.classList.remove("active");
-} )
+   
+        alldishes.forEach(element =>{
+            element.style.display = "block";
+            if(!element.classList.contains("dishes")){
+                element.style.display = "none";
+            }
+        });
+     }
+ )
+
 
 drinks.addEventListener("click",()=>{
     drinks.classList.add("active");
     allmenu.classList.remove("active");
     salad.classList.remove("active");
     dishes.classList.remove("active");
-})
+
+    alldishes.forEach(element =>{
+        element.style.display = "block";
+        if(!element.classList.contains("drink")){
+            element.style.display = "none";
+        }
+    });
+ }
+)
+
+
 
 salad.addEventListener("click",()=>{
     salad.classList.add("active");
     allmenu.classList.remove("active");
     dishes.classList.remove("active");
     drinks.classList.remove("active");
-})
+
+    alldishes.forEach(element =>{
+        element.style.display = "block";
+        if(!element.classList.contains("salad")){
+            element.style.display = "none";
+        }
+    });
+ }
+)
+
+
 allmenu.addEventListener("click",()=>{
     allmenu.classList.add("active");
     drinks.classList.remove("active");
     dishes.classList.remove("active");
     salad.classList.remove("active");
-})
+
+    alldishes.forEach(element =>{
+        element.style.display = "block";
+    });
+ }
+)
