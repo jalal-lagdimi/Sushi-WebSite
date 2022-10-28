8d847148318b56892ebe6056921cd5a3b2fd4d1b
jalal-lagdimi <jalagdimi@gmail.com>
2022-10-28 18:10:22 +0100

    commit panier

--- logic.js
+++ logic.js
@@ -1,78 +1,88 @@
-const openbtn = document.querySelector(".resp-ico");
-const closebtn = document.querySelector(".clo-ico");
-const ulc = document.querySelector('.NAVBAR');
+    const openbtn = document.querySelector(".resp-ico");
+    const closebtn = document.querySelector(".clo-ico");
+    const ulc = document.querySelector('.NAVBAR');
 
-openbtn.addEventListener("click",()=>{
-    ulc.style.right = "0"; 
-})
-closebtn.addEventListener("click",()=>{
-    ulc.style.right = "-100%"; 
-})
+    openbtn.addEventListener("click",()=>{
+        ulc.style.right = "0"; 
+    })
+    closebtn.addEventListener("click",()=>{
+        ulc.style.right = "-100%"; 
+    })
 
-const allmenu =document.getElementById("categorie-all");
-const dishes =document.getElementById("catgorie-dishes");
-const drinks =document.getElementById("catgorie-drinks");
-const salad =document.getElementById("catgorie-salad");
-const alldishes =document.querySelectorAll(".men-content");
+    const allmenu =document.getElementById("categorie-all");
+    const dishes =document.getElementById("catgorie-dishes");
+    const drinks =document.getElementById("catgorie-drinks");
+    const salad =document.getElementById("catgorie-salad");
+    const alldishes =document.querySelectorAll(".men-content");
 
 
-dishes.addEventListener("click",()=> {
-    dishes.classList.add("active");
-    allmenu.classList.remove("active");
-    drinks.classList.remove("active");
-    salad.classList.remove("active");
-   
+    dishes.addEventListener("click",()=> {
+        dishes.classList.add("active");
+        allmenu.classList.remove("active");
+        drinks.classList.remove("active");
+        salad.classList.remove("active");
+    
+            alldishes.forEach(element =>{
+                element.style.display = "block";
+                if(!element.classList.contains("dishes")){
+                    element.style.display = "none";
+                }
+            });
+        }
+    )
+
+
+    drinks.addEventListener("click",()=>{
+        drinks.classList.add("active");
+        allmenu.classList.remove("active");
+        salad.classList.remove("active");
+        dishes.classList.remove("active");
+
         alldishes.forEach(element =>{
             element.style.display = "block";
-            if(!element.classList.contains("dishes")){
+            if(!element.classList.contains("drink")){
                 element.style.display = "none";
             }
         });
-     }
- )
-
-
-drinks.addEventListener("click",()=>{
-    drinks.classList.add("active");
-    allmenu.classList.remove("active");
-    salad.classList.remove("active");
-    dishes.classList.remove("active");
-
-    alldishes.forEach(element =>{
-        element.style.display = "block";
-        if(!element.classList.contains("drink")){
-            element.style.display = "none";
-        }
-    });
- }
-)
+    }
+    )
 
 
 
-salad.addEventListener("click",()=>{
-    salad.classList.add("active");
-    allmenu.classList.remove("active");
-    dishes.classList.remove("active");
-    drinks.classList.remove("active");
+    salad.addEventListener("click",()=>{
+        salad.classList.add("active");
+        allmenu.classList.remove("active");
+        dishes.classList.remove("active");
+        drinks.classList.remove("active");
 
-    alldishes.forEach(element =>{
-        element.style.display = "block";
-        if(!element.classList.contains("salad")){
-            element.style.display = "none";
-        }
-    });
- }
-)
+        alldishes.forEach(element =>{
+            element.style.display = "block";
+            if(!element.classList.contains("salad")){
+                element.style.display = "none";
+            }
+        });
+    }
+    )
 
 
-allmenu.addEventListener("click",()=>{
-    allmenu.classList.add("active");
-    drinks.classList.remove("active");
-    dishes.classList.remove("active");
-    salad.classList.remove("active");
+    allmenu.addEventListener("click",()=>{
+        allmenu.classList.add("active");
+        drinks.classList.remove("active");
+        dishes.classList.remove("active");
+        salad.classList.remove("active");
 
-    alldishes.forEach(element =>{
-        element.style.display = "block";
-    });
- }
-)
+        alldishes.forEach(element =>{
+            element.style.display = "block";
+        });
+    }
+    )
+
+    const order_btn = document.querySelectorAll(".order");
+    const orderNum =document.querySelector(".orderNum");
+    let count =0;
+        order_btn.forEach((element)=>{
+            element.addEventListener("click",()=>{
+                count++;
+                orderNum.innerText = count;
+            }) 
+        })
